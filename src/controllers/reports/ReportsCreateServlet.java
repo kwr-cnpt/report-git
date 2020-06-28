3b2721ca364dd058660bb44ffa3c231c13e6b523
moeko.shimokawara <hr06283232@gmail.com>
2020-06-29 01:04:16 +0900

    Add Report show

--- src/controllers/reports/ReportsCreateServlet.java
+++ src/controllers/reports/ReportsCreateServlet.java
@@ -53,7 +53,7 @@ public class ReportsCreateServlet extends HttpServlet {
             r.setReport_date(report_date);
 
             r.setTitle(request.getParameter("title"));
-            r.setContent("content");
+            r.setContent(request.getParameter("content"));
 
             Timestamp currentTime = new Timestamp(System.currentTimeMillis());
             r.setCreated_at(currentTime);
